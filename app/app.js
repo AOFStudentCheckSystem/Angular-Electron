ff10c79182a2fb4294c731ab46034742db085026
DEDZTBH <peiqial@outlook.com>
2016-11-13 02:11:12 -0500

    start working on advanced and check in

--- app/app.js
+++ app/app.js
@@ -4,8 +4,9 @@ const eapp = electron.remote.app;
 const Devices = smartcard.Devices;
 const devices = new Devices();
 let currentDevices = [];
+const sqlite3 = require('sqlite3');
 
-const domain = "http://hn2.guardiantech.com.cn:57463/";
+const domain = "http://hn2.guardiantech.com.cn:10492/v2/";
 
 var app = angular.module("studentCheck",['ngRoute','routeStyles'], function ($httpProvider) {
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
@@ -54,7 +55,6 @@ app.factory("session", function () {
         }
     };
 });
-
 app.factory('httpInterceptor', ['$q', '$injector','session', function ($q, $injector ,session) {
     var httpInterceptor = {
         'responseError': function (response) {
@@ -75,6 +75,48 @@ app.factory('httpInterceptor', ['$q', '$injector','session', function ($q, $inje
     };
     return httpInterceptor;
 }]);
+app.factory('syncManager', function ($http, $rootScope) {
+    return{
+        downloadStudentInfo: function (callback) {
+            $http.get(domain + 'api/student/all').then(function (result) {
+                var stmt = $rootScope.db.prepare("INSERT OR REPLACE INTO `StudentInfo` ('id','firstName','lastName','rfid','dorm') VALUES (?,?,?,?,?)");
+                for (var i = 0; i < result.data.students.length; i++){
+                    var student = result.data.students[i];
+                    stmt.run([student.studentId,student.firstName,student.lastName,student.rfid,student.dorm]);
+                }
+                stmt.finalize();
+                callback(true);
+            }, function(error){
+                alert("Download Students Error!");
+                callback(false);
+            });
+        },
+        downloadEvents: function(callback){
+            $http.get(domain + '/api/event/list').then(function (result) {
+                var stmt = $rootScope.db.prepare("INSERT OR REPLACE INTO `Events` ('eventId','eventName','status') VALUES (?,?,?)");
+                for (var i = 0; i < result.data.events.length; i++){
+                    var event = result.data.events[i];
+                    if(event.eventStatus != 2){
+                        stmt.run([event.eventId,event.eventName,event.eventStatus]);
+                    }
+                }
+                stmt.finalize();
+                callback(true);
+            }, function(error){
+                alert("Download Events Error!");
+                callback(false);
+            });
+        },
+        downloadEventStudent: function(eventId,callback){
+            $http.get(domain + 'api/event/'+eventId+'/detail').then(function (result) {
+                callback(result.data.students);
+            }, function(error){
+                alert("Download Student @ "+eventId+" Error!");
+                callback(false);
+            });
+        }
+    };
+});
 
 app.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.interceptors.push('httpInterceptor');
@@ -88,22 +130,29 @@ app.config(function ($routeProvider) {
         })
         .when("/home",{
             templateUrl: 'templates/home.ng',
-            css: 'templates/home.css'
+            css: 'templates/home.css',
+            controller: 'homeCtrl'
         })
         .when("/event",{
             templateUrl: 'templates/event.ng',
             controller: 'eventCtrl'
         })
-        .when("/checkin/:event",{
+        .when("/checkin",{
             templateUrl: 'templates/checkin.ng',
             controller: 'checkinCtrl'
+        })
+        .when("/advanced",{
+            templateUrl: 'templates/advanced.ng',
+            css:'templates/advanced.css',
+            controller: 'advancedCtrl'
         })
         .otherwise({
             templateUrl: 'templates/index.ng',
             controller: 'indexCtrl'
         });
 });
-app.controller("navbarCtrl",function ($scope, $http, session) {
+
+app.controller("navbarCtrl",function ($scope, $http, session, $location) {
     $scope.$watchCollection(
         function () {
             return [session.get("token")!=null, session.get("username")];
@@ -113,7 +162,26 @@ app.controller("navbarCtrl",function ($scope, $http, session) {
             $scope.username = newVal[1];
         }
     );
-    $scope.goBack = function(){window.history.back();};
+    $scope.goBack = function(){
+        /*window.history.back();*/
+        var url = '';
+        switch ('/'+$location.url().split('/')[1]){
+            case '/home':
+                url = '/login';
+                break;
+            case '/event':
+                url = '/home';
+                break;
+            case '/checkin':
+                url = '/event';
+                break;
+            case '/advanced':
+                url = '/home';
+        }
+        if (url!=''){
+            $location.url(url);
+        }
+    };
 });
 app.controller('indexCtrl',function ($scope, $http, session) {
     window.location.href="#/login";
@@ -131,14 +199,97 @@ app.controller('loginCtrl',function ($scope, $http, session) {
             function (failResult) {
                 $scope.password = "";
                 $scope.isLoggingIn = false;
-                alert("Sign In Failed");
+                alert("Sign In Failed"+JSON.stringify(failResult.data));
             });
     }
 });
-app.controller('homeCtrl',function ($scope, session) {
-
+app.controller('homeCtrl',function ($rootScope) {
+    // schemaBuilder.createTable('StudentInfo')
+    //     .addColumn('id', lf.Type.INTEGER)
+    //     .addColumn('firstName', lf.Type.STRING)
+    //     .addColumn('lastName',lf.Type.STRING)
+    //     .addColumn('rfid',lf.Type.STRING)
+    //     .addColumn('dorm',lf.Type.STRING)
+    //     .addPrimaryKey(['id'])
+    //     .addNullable(['firstName','lastName','rfid','dorm']);
+    // schemaBuilder.createTable('StudentCheck')
+    //     .addColumn('id', lf.Type.INTEGER)
+    //     .addColumn('eventId', lf.Type.STRING)
+    //     .addColumn('inTime',lf.Type.STRING)
+    //     .addColumn('outTime',lf.Type.STRING)
+    //     .addColumn('upload',lf.Type.INTEGER)
+    //     .addPrimaryKey(['id','eventId'])
+    //     .addNullable(['inTime','outTime','upload']);
+    // schemaBuilder.createTable('StudentReg')
+    //     .addColumn('id', lf.Type.INTEGER)
+    //     .addColumn('rfid', lf.Type.STRING)
+    //     .addPrimaryKey(['id'])
+    //     .addNullable(['rfid']);
+    // schemaBuilder.createTable('Events')
+    //     .addColumn('eventId', lf.Type.INTEGER)
+    //     .addColumn('eventName', lf.Type.STRING)
+    //     .addColumn('eventStatus', lf.Type.INTEGER)
+    //     .addPrimaryKey(['eventId'])
+    //     .addNullable(['eventName','eventStatus']);
+    // // var db;
+    // // var item;
+    // // schemaBuilder.connect()
+    // //     .then(function(dbR){
+    // //     db = dbR;
+    // //     item = db.getSchema().table('StudentInfo');
+    // //     var row = item.createRow({
+    // //         'id': 12345,
+    // //         'firstName': 'Tony',
+    // //         'lastName': 'Liu',
+    // //         'rfid': 'nil',
+    // //         'dorm': 'ELE233'
+    // //     });
+    // //     return db.insertOrReplace().into(item).values([row]).exec();
+    // // })
+    // //     .then(function() {
+    // //     return db.select().from(item).where(item.id.eq(12345)).exec();
+    // // }).then(function(results) {
+    // //     results.forEach(function(row) {
+    // //         console.log(row['lastName'] + row['firstName']);
+    // //     });
+    // // });
+    // schemaBuilder.connect().then(function (dbR) {
+    //     $rootScope.db = dbR;
+    // });
+    $rootScope.db = new sqlite3.Database('AOFCheckDB.db', function (error) {
+        if (error!=null) alert("Failed to initialize database! " + error);
+        else {
+            // console.log('DB init succeed');
+            $rootScope.db.exec(
+                "CREATE TABLE if not exists StudentInfo      " +
+                "(id     TEXT PRIMARY KEY UNIQUE NOT NULL," +
+                " firstName TEXT                            ," +
+                " lastName  TEXT                            ," +
+                " rfid      TEXT                            ," +
+                " dorm      TEXT                           );" +
+                "CREATE TABLE if not exists StudentCheck     " +
+                "(id     TEXT                    NOT NULL," +
+                " eventId   TEXT                    NOT NULL," +
+                " inTime    TEXT                            ," +
+                " outTime   TEXT                            ," +
+                " upload  TEXT                              ," +
+                " PRIMARY KEY (id, eventId)             );" +
+                "CREATE TABLE if not exists StudentReg       " +
+                "(id     TEXT PRIMARY KEY UNIQUE NOT NULL," +
+                " rfid      TEXT                           );" +
+                "CREATE TABLE if not exists Events           " +
+                "(eventId   TEXT PRIMARY KEY UNIQUE NOT NULL," +
+                " eventName TEXT                            ," +
+                " status    TEXT                           ) ",
+            function(error){
+                if (error!=null) alert("Failed to create table! " + error);
+                // else console.log("Create table succeed");
+            });
+        }
+    });
 });
-app.controller('eventCtrl',function ($scope, $http) {
+app.controller('eventCtrl',function ($scope, $http, session) {
+    $scope.selected = -1;
     $scope.events = [];
     $http.get(domain+"api/event/list").then(function (successReturn) {
         $scope.events = successReturn.data.events;
@@ -150,14 +301,62 @@ app.controller('eventCtrl',function ($scope, $http) {
         return $scope.selected == item;
     };
     $scope.continueEvent = function () {
-        window.location.href = "#/checkin/"+JSON.stringify($scope.events[$scope.selected]);
-        //console.log("#/checkin/"+JSON.stringify($scope.events[$scope.selected]));
+        if($scope.selected < 0){
+            alert("Please select a event!");
+        }else {
+            session.set('currentEvent',JSON.stringify($scope.events[$scope.selected]));
+            window.location.href = "#/checkin";
+        }
+    };
+    $scope.activeFilter = function(event) {
+        return event.eventStatus != 2;
+    };
+});
+app.controller('checkinCtrl',function ($scope, session, syncManager, $rootScope) {
+    $scope.q = '';
+    $scope.event = JSON.parse(session.get('currentEvent'));
+    console.log("receive:"+$scope.event.eventId);
+    $scope.students = [];
+    syncManager.downloadEventStudent($scope.event.eventId, function (ret) {
+        if (ret != false){
+            for (var i = 0; i < ret.length; i++){
+                const j = i;
+                $rootScope.db.get("SELECT * FROM `StudentInfo` WHERE `id` = ?", [ret[j].studentId], function (error, rowa) {
+                    ret[j].firstName = rowa.firstName;
+                    ret[j].lastName = rowa.lastName;
+                    $scope.students.push(ret[j]);
+                    $scope.$apply();
+                });
+            }
+        }
+        // if (ret!=false){
+        //     var fn,ln;
+        //     for (var i = 0; i < ret.length; i++){
+        //         const j = i;
+        //         $rootScope.db.get("SELECT * FROM `StudentInfo` WHERE `id` = ?",[ret[i].studentId],function (error,rowa) {
+        //             fn = rowa.firstName;
+        //             ln = rowa.lastName;
+        //             ret[j]['firstName'] = fn;
+        //             ret[j]['lastName'] = ln;
+        //             $scope.students = ret;
+        //             console.log($scope.students);
+        //         });
+        //     }
+        // }
+
+    });
+    $scope.searchFilter = function(student){
+        return student.lastName.substring(0,$scope.q.length).toLowerCase() === $scope.q.toLowerCase();
     }
 });
-app.controller('checkinCtrl',['$routeParams','$scope',function ($routeParams,$scope) {
-    $scope.event = JSON.parse($routeParams.event);
-    console.log("receive:"+$scope.event.eventId);
-}]);
+app.controller('advancedCtrl',function($scope,syncManager){
+    $scope.downloadStudentInfo = function () {
+        syncManager.downloadStudentInfo(function(ret){});
+    };
+    $scope.downloadEvents = function(){
+        syncManager.downloadEvents(function(ret){});
+    };
+});
 // app.controller("cardDisplayCtrl",function ($scope) {
 //     $scope.card = "No Reader";
 //     $scope.card1 = "NN";
